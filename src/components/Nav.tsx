9db86f1fd73da3bdcd11ebd02f11701e686963c8
John Dunn <johnwdunn20@gmail.com>
2024-02-15 13:14:57 -0800

    added preliminary search route

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,7 +7,7 @@ const Nav = () => {
     <div>
       <h1>Nav Bar Test</h1>
       {/* Tells nextauth to use descope as the login provider and when finished, go back to home page */}
-      <button onClick={() => signIn("descope", { callbackUrl: "/" })}>Sign In</button>
+      <button onClick={() => signIn("descope", { callbackUrl: "/" })}>Sign Up or Sign In</button>
     </div>
   )
 }
