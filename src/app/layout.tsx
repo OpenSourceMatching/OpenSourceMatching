fe2c8e30adf0b832dd726fd7144b892a489b994a
Prashay <113265973+wadesword89@users.noreply.github.com>
2024-02-16 10:33:57 -0800

    clean up layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import Footer from "@components/Footer";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import NextAuthSessionProvider from "@components/NextAuthSessionProvider";
-import Nav from "@components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,7 +28,7 @@ export default function RootLayout({
             <Navbar />
               {children}
             <Footer />
-            </StyledComponentsRegistry>
+          </StyledComponentsRegistry>
         </NextAuthSessionProvider>
           {/* <Analytics/>
           <SpeedInsights/> */}
